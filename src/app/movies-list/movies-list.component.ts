fb78be1ff34a02d69fa63fe732d28d29eb7c096b
Rajeev Ranjan <rajeev.ranjan@hitachivantara.com>
2020-08-13 01:36:24 +0530

    style updated

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -52,4 +52,9 @@ export class MoviesListComponent implements OnInit {
     });
   }
 
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
